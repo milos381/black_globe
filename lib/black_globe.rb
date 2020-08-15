d5fe3542e0fb2abddf43898e16615014d89b1666
milos381 <65251684+milos381@users.noreply.github.com>
2020-08-15 14:43:39 -0400

    Update black_globe.rb

--- lib/black_globe.rb
+++ lib/black_globe.rb
@@ -1,6 +1,22 @@
 require "black_globe/version"
+require "open-uri"
+require 'net/http'
+require 'json'
 
-module BlackGlobe
-  class Error < StandardError; end
-  # Your code goes here...
+
+class BlackGlobe
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get(uri)
+    response.body
+  end
+  def parse_json
+    result = JSON.parse(self.get_response_body)
+    rate = result['conversion_rates']['EUR']
+  end
 end
+get_rate = BlackGlobe.new('"https://prime.exchangerate-api.com/v5/33abda6f0da266a7c858930b/latest/USD"')
