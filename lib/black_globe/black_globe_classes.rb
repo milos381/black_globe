cc988d0ef1967ceef28d82f53520c1782a164cf5
Milos Popovic <milos_popovic@outlook.com>
2020-08-17 21:13:52 +0000

    trying to class

--- lib/black_globe/black_globe_classes.rb
+++ lib/black_globe/black_globe_classes.rb
@@ -2,7 +2,7 @@ class BlackGlobe::Cli
   
   attr_accessor :country_one, :country_two, :input_value
   
-  def initialize(input, country)
+  def initialize(country_one, country_two, input_value)
     @country_one = country_one
     @country_two = country_two
     @input_value = input_value
@@ -11,18 +11,18 @@ class BlackGlobe::Cli
   
   def selection_one
   puts "Please type the country of the currency you currently hold:"
-  input = gets.strip
+    country_one = gets.strip
   end 
   
   def selection_two
     puts "Now type a country whose currency you would like to exchange for:"
-    input = gets.strip
+    country_two = gets.strip
   end
   
   def money_value
     puts "Now type the amount of money you would like to exchange:"
-    input = gets.strip
-    input.to_i
+    input_value = gets.strip
+    input_value.to_i
   end
   
   def selection_one_proceed
@@ -34,10 +34,6 @@ class BlackGlobe::Cli
     end
   end
   
-  def valid_country_one?
-    
-  end
-  
   def selection_two_proceed
     if country_array.include?(selection_two)
       money_value
@@ -46,19 +42,4 @@ class BlackGlobe::Cli
       selection_two
     end
   end
-  
-  until country_array.include?(selection_one)
-    puts "...ops, something went wrong. Let's try again:"
-    selection_one
-  end
-  if country_array.include?(selection_one)
-    puts "Great!"
-    selection_two
-  else
-  end
-  
-  if country_array.include?(selection_two)
-    puts "Great!"
-    money_value
-  end  
 end
