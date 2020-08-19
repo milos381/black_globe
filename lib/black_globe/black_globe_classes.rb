8ee0188e051c1d64b77f195aa1c01aef93e66d14
Milos Popovic <milos_popovic@outlook.com>
2020-08-19 04:02:08 +0000

    finish currency_db

--- lib/black_globe/black_globe_classes.rb
+++ lib/black_globe/black_globe_classes.rb
@@ -1,9 +1,9 @@
 class CurrencyExchanger
 
-  class CountryNotSupported < StandardError; end
+  class CurrencyNotSupported < StandardError; end
 
   def initialize(from, to, money)
-    #[from, to].each { |e| raise CountryNotSupported if supported_countries.exclude? e }
+    [from, to].each { |e| raise CurrencyNotSupported if supported_currency.exclude? e }
     
     
     @from = from
@@ -11,10 +11,63 @@ class CurrencyExchanger
     @money = money
   end
 
-  # def supported_countries
-  #   YAML.load_file( '/your_path_to_country_yaml' )
-  #   # set countries in yml file, not initialize values in class explicitly 
-  # end
+  def supported_currency
+  array = [
+"AED	UAE Dirham",
+"ARS	Argentine Peso",
+"AUD	Australian Dollar",
+"BGN	Bulgarian Lev",
+"BRL	Brazilian Real",
+"BSD	Bahamian Dollar",
+"CAD	Canadian Dollar",
+"CHF	Swiss Franc",
+"CLP	Chilean Peso",
+"CNY	Chinese Renminbi",
+"COP	Colombian Peso",
+"CZK	Czech Koruna",
+"DKK	Danish Krone",
+"DOP	Dominican Peso",
+"EGP	Egyptian Pound",
+"EUR	Euro",
+"FJD	Fiji Dollar",
+"GBP	Pound Sterling",
+"GTQ	Guatemalan Quetzal",
+"HKD	Hong Kong Dollar",
+"HRK	Croatian Kuna",
+"HUF	Hungarian Forint",
+"IDR	Indonesian Rupiah",
+"ILS	Israeli New Shekel",
+"INR	Indian Rupee",
+"ISK	Icelandic Krona",
+"JPY	Japanese Yen",
+"KRW	South Korean Won",
+"KZT	Kazakhstani Tenge",
+"MVR	Maldivian Rufiyaa",
+"MXN	Mexican Peso",
+"MYR	Malaysian Ringgit",
+"NOK	Norwegian Krone",
+"NZD	New Zealand Dollar",
+"PAB	Panamanian Balboa",
+"PEN	Peruvian Sol",
+"PHP	Philippine Peso",
+"PKR	Pakistani Rupee",
+"PLN	Polish Zloty",
+"PYG	Paraguayan Guarani",
+"RON	Romanian Leu",
+"RUB	Russian Ruble",
+"SAR	Saudi Riyal",
+"SEK	Swedish Krona",
+"SGD	Singapore Dollar",
+"THB	Thai Baht",
+"TRY	Turkish Lira",
+"TWD	New Taiwan Dollar",
+"UAH	Ukrainian Hryvnia",
+"USD	United States Dollar",
+"UYU	Uruguayan Peso",
+"ZAR	South African Rand"
+
+    ]
+  end
 
   def exchange_rate
     # implement exchange rate depends on @from and @to
