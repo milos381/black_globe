7b983309952f2328ccba059f19188a989c3c335f
Milos Popovic <milos_popovic@outlook.com>
2020-08-19 02:23:33 +0000

    edit greeting

--- lib/black_globe/greeting.rb
+++ lib/black_globe/greeting.rb
@@ -1,6 +1,66 @@
-class Greet
+def initial_greeting
+  array = [
+"Code	Currency Name	Country",
+"1. AED	UAE Dirham	United Arab Emirates",
+"2. ARS	Argentine Peso	Argentina",
+"3. AUD	Australian Dollar	Australia",
+"4. BGN	Bulgarian Lev	Bulgaria",
+"5. BRL	Brazilian Real	Brazil",
+"6. BSD	Bahamian Dollar	Bahamas",
+"7. CAD	Canadian Dollar	Canada",
+"8. CHF	Swiss Franc	Switzerland",
+"9. CLP	Chilean Peso	Chile",
+"10. CNY	Chinese Renminbi	China",
+"11. COP	Colombian Peso	Colombia",
+"12. CZK	Czech Koruna	Czech Republic",
+"13. DKK	Danish Krone	Denmark",
+"14. DOP	Dominican Peso	Dominican Republic",
+"15. EGP	Egyptian Pound	Egypt",
+"16. EUR	Euro	European Union",
+"17. FJD	Fiji Dollar	Fiji",
+"18. GBP	Pound Sterling	United Kingdom",
+"19. GTQ	Guatemalan Quetzal	Guatemala",
+"20. HKD	Hong Kong Dollar	Hong Kong",
+"21. HRK	Croatian Kuna	Croatia",
+"22. HUF	Hungarian Forint	Hungary",
+"23. IDR	Indonesian Rupiah	Indonesia",
+"24. ILS	Israeli New Shekel	Israel",
+"25. INR	Indian Rupee	India",
+"26. ISK	Icelandic Krona	Iceland",
+"27. JPY	Japanese Yen	Japan",
+"28. KRW	South Korean Won	South Korea",
+"29. KZT	Kazakhstani Tenge	Kazakhstan",
+"30. MVR	Maldivian Rufiyaa	Maldives",
+"31. MXN	Mexican Peso	Mexico",
+"32. MYR	Malaysian Ringgit	Malaysia",
+"33. NOK	Norwegian Krone	Norway",
+"34. NZD	New Zealand Dollar	New Zealand",
+"35. PAB	Panamanian Balboa	Panama",
+"36. PEN	Peruvian Sol	Peru",
+"37. PHP	Philippine Peso	Philippines",
+"38. PKR	Pakistani Rupee	Pakistan",
+"39. PLN	Polish Zloty	Poland",
+"40. PYG	Paraguayan Guarani	Paraguay",
+"41. RON	Romanian Leu	Romania",
+"42. RUB	Russian Ruble	Russia",
+"43. SAR	Saudi Riyal	Saudi Arabia",
+"44. SEK	Swedish Krona	Sweden",
+"45. SGD	Singapore Dollar	Singapore",
+"46. THB	Thai Baht	Thailand",
+"47. TRY	Turkish Lira	Turkey",
+"48. TWD	New Taiwan Dollar	Taiwan",
+"49. UAH	Ukrainian Hryvnia	Ukraine",
+"50. USD	United States Dollar	United States",
+"51. UYU	Uruguayan Peso	Uruguay",
+"52. ZAR	South African Rand	South Africa"
 
+    ]
+  puts "Welcome to Black Globe, an online currency exchange office"
+  puts ""
+  puts "In order to proceed, please enter currencies for transaction as they appear in the list below:"
+  puts array
+  
 end
 
-
+initial_greeting
 
